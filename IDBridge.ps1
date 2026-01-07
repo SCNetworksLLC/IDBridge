aa44a8d8c6ba3320bcb8362900416fb9ff84b1df
cattanach-mfld <53875551+cattanach-mfld@users.noreply.github.com>
2026-01-07 14:56:36 -0600

    Added BuildingID

--- IDBridge.ps1
+++ IDBridge.ps1
@@ -113,7 +113,8 @@ if ($IDConfig.Student.Enabled -eq $true -and $IDConfig.Student.SourceType -eq "S
             Username             = $_.DisplayId
             #Building             = (Get-Culture).TextInfo.ToTitleCase($($_.SchoolName).ToLower())
             Building             = if ($IDConfig.Student.BuildingIDMapping[$_.SchoolID].Name) {$IDConfig.Student.BuildingIDMapping."$_.SchoolID".Name} else {$IDConfig.Student.BuildingIDMapping."000".Name}
-            BuildingID           = if ($IDConfig.Student.BuildingIDMapping[$_.SchoolID].Code) {$IDConfig.Student.BuildingIDMapping."$_.SchoolID".Code} else {$IDConfig.Student.BuildingIDMapping."000".Code}
+            BuildingCode         = if ($IDConfig.Student.BuildingIDMapping[$_.SchoolID].Code) {$IDConfig.Student.BuildingIDMapping."$_.SchoolID".Code} else {$IDConfig.Student.BuildingIDMapping."000".Code}
+            BuildingID           = $_.SchoolID
             Grade                = (Get-StudentGrade -gradYear $_.GradYr -gradeAdvanceDate $IDConfig.Student.GradeAdvanceDate)
             GradYear             = $_.GradYr
             JobTitle             = "Student - Grade $(Get-StudentGrade -gradYear $_.GradYr -gradeAdvanceDate $IDConfig.Student.GradeAdvanceDate)"
